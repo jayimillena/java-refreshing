ac58b772d64dfce562b5b214e11eea7ca217225c
Two <22344029+jayimillena@users.noreply.github.com>
2023-08-28 21:24:07 +0800

    Initial commit

---
+++ JavaProj.java/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        String firstNumber = "0";
+        String secondNumber = "2";
+
+        System.out.print("The sum of " + firstNumber + " and " + secondNumber + " is " + (Double.parseDouble(firstNumber) + Double.parseDouble(secondNumber)));
+    }
+}
